cafcf5f7ff54ce64418779eca39a0ea383da5b07
huizhangyue <15011269379@163.com>
2019-12-13 14:12:09 +0800

    组测试

--- Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass1.java
+++ Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass1.java
@@ -1,4 +1,13 @@
 package com.course.testng.groups;
 
+import org.testng.annotations.Test;
+
+@Test(groups = "stu")
 public class GroupsOnClass1 {
+    public void stu1(){
+       System.out.println("GrouopsOnClass1中的stu11111运行");
+    }
+    public void stu2(){
+        System.out.println("GrouopsOnClass1中的stu22222运行");
+    }
 }
--- Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass3.java
+++ Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass3.java
@@ -1,4 +1,13 @@
 package com.course.testng.groups;
 
+import org.testng.annotations.Test;
+
+@Test(groups = "teacher")
 public class GroupsOnClass3 {
+    public void teacher1(){
+        System.out.println("GrouopsOnClass3中的teacher11111运行");
+    }
+    public void teacher2(){
+        System.out.println("GrouopsOnClass3中的teacher22222运行");
+    }
 }
