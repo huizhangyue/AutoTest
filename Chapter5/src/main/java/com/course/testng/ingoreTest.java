40409a0dac40876a18041d4b663d784998c5ccb1
huizhangyue <15011269379@163.com>
2019-12-13 15:29:51 +0800

    忽略测试

--- Chapter5/src/main/java/com/course/testng/ingoreTest.java
+++ Chapter5/src/main/java/com/course/testng/ingoreTest.java
@@ -1,4 +1,18 @@
 package com.course.testng;
 
+import org.testng.annotations.Test;
+
 public class ingoreTest {
+    @Test(enabled = false)
+    public  void  ignore1(){
+           System.out.println("ignore1 执行！");
+       }
+    @Test(enabled = true)
+    public  void  ignore2(){
+        System.out.println("ignore1 不执行！");
+    }
+    @Test(enabled = false)
+    public  void  ignore3(){
+        System.out.println("ignore1 执行！");
+    }
 }
