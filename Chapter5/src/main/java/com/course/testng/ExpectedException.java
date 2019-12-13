0190f455fee6e515b34ec3ae1e2219ec02cf0dc2
huizhangyue <15011269379@163.com>
2019-12-13 15:29:13 +0800

    抛出异常测试

--- Chapter5/src/main/java/com/course/testng/ExpectedException.java
+++ Chapter5/src/main/java/com/course/testng/ExpectedException.java
@@ -1,4 +1,19 @@
 package com.course.testng;
 
+import org.testng.annotations.Test;
+
 public class ExpectedException {
+    /**
+     * 什么时候用到异常测试
+     * */
+    @Test(expectedExceptions = RuntimeException.class)
+    public void runTimeExceptionFailed(){
+        System.out.println("这是一个失败的异常测试");
+    }
+    //这是一个成功异常测试
+    @Test(expectedExceptions = RuntimeException.class)
+    public void runTimeExceptionSuccess(){
+        System.out.println("这是一个成功的异常测试");
+        throw new RuntimeException();
+    }
 }
